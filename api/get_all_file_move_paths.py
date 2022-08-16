9bab217b73460bd236569059c69f70cc4917bfd0
ObamaTheLlama114 <william.chomel@gmail.com>
2022-08-16 07:11:59 -0500

    add get_all_file_move_paths api endpoint

---
+++ api/get_all_file_move_paths.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+
+from http.server import BaseHTTPRequestHandler
+from datetime import datetime
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        client = MongoClient(
+            "mongodb+srv://username:secretpass@migrationdata.1ptf869.mongodb.net/?retryWrites=true&w=majority"
+        )
+        entries = client.data.file_move_paths.find()
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(str({entry["current"]: entry["new"] for entry in entries}))
+        return
+        
