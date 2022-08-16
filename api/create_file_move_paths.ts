dc2a04bd624a5128ad027de7dbb59d519fbc60df
ObamaTheLlama114 <william.chomel@gmail.com>
2022-08-16 09:35:26 -0500

    fix

--- api/create_file_move_paths.ts
+++ api/create_file_move_paths.ts
@@ -2,6 +2,12 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 import * as dotenv from 'dotenv';
 import { MongoClient } from 'mongodb';
 
+interface FileMovePaths {
+	current: string;
+	new: string;
+	name?: string;
+}
+
 export default (request: VercelRequest, response: VercelResponse) => {
 	dotenv.config();
 	const client = new MongoClient(process.env.MONGODB_URI || "");
@@ -11,12 +17,12 @@ export default (request: VercelRequest, response: VercelResponse) => {
 		try {
 			let body = JSON.parse(request.body);
 			for (let i = 0; i < body.data.length; i++) {
-				let data = {
+				let data: FileMovePaths = {
 					"current": body.data[i]["current"],  // required current path
 					"new": body.data[i]["new"]  // required new path
 				}
 				if ("name" in body["data"][i]) {
-					data["name"] = body.data[i]["name"];  // Optional name
+					data.name = body.data[i]["name"];  // optional name
 				}
 				collection.insertOne(data);
 			}
