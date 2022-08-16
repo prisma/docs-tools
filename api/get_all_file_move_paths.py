a6b3055dae367d99582a2dde4cfaee329dabcd1d
ObamaTheLlama114 <william.chomel@gmail.com>
2022-08-16 08:15:55 -0500

    testing

--- api/get_all_file_move_paths.py
+++ api/get_all_file_move_paths.py
@@ -7,10 +7,5 @@ from datetime import datetime
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         client = MongoClient(os.environ.get("MONGODB_URI"))
-        entries = client.data.file_move_paths.find()
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        self.wfile.write(str({entry["current"]: entry["new"] for entry in entries}))
         return
         
