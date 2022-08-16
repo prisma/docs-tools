9b6a7fe738e1b93eb6e0b9335d4f07afe25b2343
ObamaTheLlama114 <william.chomel@gmail.com>
2022-08-16 08:03:30 -0500

    fixed mongodb uri

--- api/get_all_file_move_paths.py
+++ api/get_all_file_move_paths.py
@@ -1,3 +1,4 @@
+import os
 from pymongo import MongoClient
 
 from http.server import BaseHTTPRequestHandler
@@ -5,9 +6,7 @@ from datetime import datetime
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        client = MongoClient(
-            "mongodb+srv://username:secretpass@migrationdata.1ptf869.mongodb.net/?retryWrites=true&w=majority"
-        )
+        client = MongoClient(os.environ["MONGODB_URI"])
         entries = client.data.file_move_paths.find()
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
