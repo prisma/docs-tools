b3855c1d820ae25efd6f12cc19ec21734f09fb38
ObamaTheLlama114 <william.chomel@gmail.com>
2022-08-16 07:52:19 -0500

    revert update get_all_file_move_paths

--- api/get_all_file_move_paths.py
+++ api/get_all_file_move_paths.py
@@ -6,12 +6,12 @@ from datetime import datetime
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         client = MongoClient(
-            "mongodb+srv://username:secretpass@migrationdata.1ptf869.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1')
+            "mongodb+srv://username:secretpass@migrationdata.1ptf869.mongodb.net/?retryWrites=true&w=majority"
         )
         entries = client.data.file_move_paths.find()
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write(str(entries))
+        self.wfile.write(str({entry["current"]: entry["new"] for entry in entries}))
         return
         
