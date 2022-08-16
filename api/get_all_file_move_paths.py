e39320236f7a5c1e51a23f7282485998b362c3f3
ObamaTheLlama114 <william.chomel@gmail.com>
2022-08-16 08:25:18 -0500

    did stuff

--- api/get_all_file_move_paths.py
+++ api/get_all_file_move_paths.py
@@ -1,12 +1,12 @@
 import os
-from pymongo import MongoClient
+from pymongo import MongoClient, ServerApi
 
 from http.server import BaseHTTPRequestHandler
 from datetime import datetime
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        client = MongoClient(os.environ.get("MONGODB_URI"))
+        client = MongoClient(os.environ.get("MONGODB_URI"), server_api=ServerApi('1'), serverSelectionTimeoutMS=5000)
         entries = client.data.file_move_paths.find()
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
