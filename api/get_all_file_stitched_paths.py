e6b05b9341f7125600c35ccf5063c67763cb13ef
ObamaTheLlama114 <william.chomel@gmail.com>
2022-08-16 22:01:39 -0500

    fix

--- api/get_all_file_stitched_paths.py
+++ api/get_all_file_stitched_paths.py
@@ -9,13 +9,18 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','application/json')
         self.end_headers()
-        self.wfile.write(json.dumps([{
-            {"name": i["name"]} if "name" in i.keys() else None: None,
+        data = [{
+            "name": i["name"] if "name" in i.keys() else None,
             "dest": i["dest"],
             "header": i["header"],
             "body": [{
                 "key": client.data.file_surgery_paths.find_one({"_id": j["key"]})["new"],
                 "index": j["index"]
                 } for j in i["body"]]
-            } for i in client.data.file_stitched_paths.find()]).encode())
+            } for i in client.data.file_stitched_paths.find()]
+        for i in data:
+            for x, y in i.items():
+                if y == None:
+                    del i[x]
+        self.wfile.write(json.dumps(data).encode())
         return
