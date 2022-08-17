f6dc33538cb3ac5c806146aafc8a848cfba14335
ObamaTheLlama114 <william.chomel@gmail.com>
2022-08-16 21:34:26 -0500

    afsdasfd

--- api/get_all_file_stitched_paths.py
+++ api/get_all_file_stitched_paths.py
@@ -10,5 +10,13 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','application/json')
         self.end_headers()
-        self.wfile.write([{"name": i["name"], "dest": i["dest"], "header": i["header"], "body": [{"key": client.data.file_stitched_paths.find_one({"_id": j["key"]}), "index": j["index"]} for j in i["body"]]} for i in client.data.file_stitched_paths.find()])
+        self.wfile.write([{
+            "name": i["name"],
+            "dest": i["dest"],
+            "header": i["header"],
+            "body": [{
+                "key": client.data.file_stitched_paths.find_one({"_id": j["key"]})["new"],
+                "index": j["index"]
+                } for j in i["body"]]
+            } for i in client.data.file_stitched_paths.find()])
         return
