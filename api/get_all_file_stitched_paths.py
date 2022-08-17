b2840f7267566b34a40b53eecb81a985c96ce619
ObamaTheLlama114 <william.chomel@gmail.com>
2022-08-16 21:13:26 -0500

    testing

---
+++ api/get_all_file_stitched_paths.py
@@ -0,0 +1,14 @@
+from http.server import BaseHTTPRequestHandler
+import os
+from pymongo import MongoClient
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        import os
+        from pymongo import MongoClient
+        client = MongoClient(os.environ['MONGODB_URI'])
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write("testing 123")
+        return
