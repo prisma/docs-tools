bf53625eb578bd220abd6f277cb04c36a8f93bf9
ObamaTheLlama114 <william.chomel@gmail.com>
2022-08-16 21:46:38 -0500

    fasd

--- api/get_all_file_stitched_paths.py
+++ api/get_all_file_stitched_paths.py
@@ -1,16 +1,15 @@
 from http.server import BaseHTTPRequestHandler
 import os
 from pymongo import MongoClient
+import json
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        import os
-        from pymongo import MongoClient
         client = MongoClient(os.environ['MONGODB_URI'])
         self.send_response(200)
         self.send_header('Content-type','application/json')
         self.end_headers()
-        self.wfile.write([{
+        self.wfile.write(json.dumps([{
             "name": i["name"],
             "dest": i["dest"],
             "header": i["header"],
@@ -18,5 +17,5 @@ class handler(BaseHTTPRequestHandler):
                 "key": client.data.file_surgery_paths.find_one({"_id": j["key"]})["new"],
                 "index": j["index"]
                 } for j in i["body"]]
-            } for i in client.data.file_stitched_paths.find()])
+            } for i in client.data.file_stitched_paths.find()]))
         return
