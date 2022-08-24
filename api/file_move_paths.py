71902cb5aeee77a19cceb15ca01d10b0fc2bc027
ObamaTheLlama114 <william.chomel@gmail.com>
2022-08-24 10:18:57 -0500

    test

--- api/file_move_paths.py
+++ api/file_move_paths.py
@@ -44,10 +44,10 @@ def catch_all(path):
     elif request.method == 'GET':
         if request.headers.get('Content-Type') != 'application/json': return Response("Content-Type must be application/json", mimetype='text/plain', status=400)
         body = request.json
-        try:
-            return Response(json.dumps([i for i in client.data.file_move_paths.find(validate_query(body, move_type))]), mimetype='application/json')
-        except:
-            return Response(json.dumps({"Error": "bad shape"}), mimetype='application/json')
+        #try:
+        return Response(json.dumps(client.data.file_move_paths.find(validate_query(body, move_type))), mimetype='application/json')
+        #except:
+            #return Response(json.dumps({"Error": "bad shape"}), mimetype='application/json')
 
     elif request.method == 'POST':
         if request.headers.get('Content-Type') != 'application/json': return Response("Content-Type must be application/json", mimetype='text/plain', status=400)
