40181d55825872ce42fb790603f96d3c399625d8
ObamaTheLlama114 <william.chomel@gmail.com>
2022-08-24 12:48:18 -0500

    fix

--- api/file_delete_paths.py
+++ api/file_delete_paths.py
@@ -1,6 +1,7 @@
 from http.server import BaseHTTPRequestHandler
 import os
 from pymongo import MongoClient
+from bson import ObjectId
 import json
 from flask import Flask, Response
 from api._util.validate import *
@@ -43,7 +44,7 @@ def catch_all(path):
     
     elif request.method == 'GET':
         try:
-            return Response(json.dumps([{key:str(value) if key is "_id" else value for key, value in item.items()} for item in list(client.data.file_delete_paths.find(validate_query(body, delete_type)))]), mimetype='application/json')
+            return Response(json.dumps([{key:str(value) if type(value) is ObjectId else value for key, value in item.items()} for item in list(client.data.file_delete_paths.find(validate_query(body, delete_type)))]), mimetype='application/json')
         except:
             return Response(json.dumps({"Error": "bad shape"}), mimetype='application/json')
     
--- api/file_move_paths.py
+++ api/file_move_paths.py
@@ -44,7 +44,7 @@ def catch_all(path):
     
     elif request.method == 'GET':
         #try:
-            return Response(json.dumps([{key:str(value) if key is "_id" else value for key, value in item.items()} for item in list(client.data.file_move_paths.find(validate_query(body, move_type)))]), mimetype='application/json')
+            return Response(json.dumps([{key:str(value) if type(value) is ObjectId else value for key, value in item.items()} for item in list(client.data.file_move_paths.find(validate_query(body, move_type)))]), mimetype='application/json')
         #except:
             #return Response(json.dumps({"Error": "bad shape"}), mimetype='application/json')
 
