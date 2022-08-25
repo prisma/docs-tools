db8a0b31c8f7ff47bc29142f994bfc6210f14fd7
ObamaTheLlama114 <william.chomel@gmail.com>
2022-08-25 10:08:35 -0500

    refactored surgery and stitched GET

--- api/file_stitched_paths.py
+++ api/file_stitched_paths.py
@@ -9,8 +9,19 @@ app = Flask(__name__)
 @app.route('/<path:path>', methods=['PUT', 'GET', 'DELETE'])
 def catch_all(path):
     from flask import request
+
+    stitched_type = {
+        "_id": [str, None],
+        "name": [str, None],
+        "new_path": [str],
+        "header": [dict],
+        "body": [list],
+    }
     
     client = MongoClient(os.environ['MONGODB_URI'])
+    
+    if request.headers.get('Content-Type') != 'application/json': return Response("Content-Type must be application/json", mimetype='text/plain', status=400)
+    body = request.json
     
     if request.method == 'PUT':
         if request.headers.get('Content-Type') != 'application/json': return Response("Content-Type must be application/json", mimetype='text/plain', status=400)
@@ -38,31 +49,12 @@ def catch_all(path):
         return Response(json.dumps(response), mimetype='application/json', status=200)
     
     elif request.method == 'GET':
-        if request.headers.get('Content-Type') != 'application/json': return Response("Content-Type must be application/json", mimetype='text/plain', status=400)
-        body = request.json
-        args = {}
-        if "_id" in body.keys():
-            args["_id"] = body["_id"]
-        if "name" in body.keys():
-            args["name"] = body["name"]
-        if "dest" in body.keys():
-            args["dest"] = body["dest"]
-        data = [{
-            "_id": str(i["_id"]),
-            "name": i["name"] if "name" in i.keys() else None,
-            "dest": i["dest"],
-            "header": i["header"],
-            "body": [{
-                "key": client.data.file_surgery_paths.find_one({"_id": j["key"]})["new"],
-                "index": j["index"]
-                } for j in i["body"]]
-            } for i in client.data.file_stitched_paths.find(args)]
-        client.data.changes.insert_one({})
-        return Response(json.dumps([{j:i[j] for j in i.keys() if i[j] != None} for i in data]), mimetype='application/json')
+        try:
+            return Response(json.dumps([{key:str(value) if type(value) is ObjectId else [{"index": bodypart["index"], "key": client.data.file_surgery_paths.find_one(bodypart["key"])["new"]} for bodypart in value] if key is body else value for key, value in item.items()} for item in list(client.data.file_stitched_paths.find(validate_query(body, stitched_type)))]), mimetype='application/json')
+        except:
+            return Response(json.dumps({"Error": "bad shape"}), mimetype='application/json')
     
     elif request.method == 'DELETE':
-        if request.headers.get('Content-Type') != 'application/json': return Response("Content-Type must be application/json", mimetype='text/plain', status=400)
-        body = request.json
         args = {}
         if "_id" in body.keys():
             args["_id"] = body["_id"]
