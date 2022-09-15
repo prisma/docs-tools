bad2fb0ce7a9f43c35fccda6aade0ee86ebd0e8d
Bama <44502143+ObamaTheLlama114@users.noreply.github.com>
2022-09-15 09:23:37 -0500

    URI to URL (#26)
    * added POST delete
    
    * refactoring api
    
    * refactoring api again
    
    * moree refactoring
    
    * fix get move
    
    * fix validate
    
    * add find to get move
    
    * FIX
    
    * fix
    
    * test
    
    * fix
    
    * test
    
    * fix
    
    * fix
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * refactored get delete
    
    * test
    
    * fix
    
    * fix
    
    * fix
    
    * added error handling to get move
    
    * refactored get surgery
    
    * fix
    
    * test
    
    * fix
    
    * refactored surgery and stitched GET
    
    * testing
    
    * fix
    
    * test fix GET stitch
    
    * testing refactored PUT move
    
    * changed name of apispec file
    
    * testing
    
    * fix
    
    * fix
    
    * fix
    
    * change None to NoneType in API
    
    * fixed nonetype
    
    * fix
    
    * fix
    
    * clean up testing stuff
    
    * refactored put for delete and surgery
    
    * testing PUT stitch
    
    * fix
    
    * is to ==
    
    * fix
    
    * fix
    
    * fix
    
    * refactored DELETE move
    
    * refactored DELETE delete and surgery
    
    * refactor DELETE stitch
    
    * added POST move
    
    * fix
    
    * added POST
    
    * fix
    
    * changed stuff
    
    * URI to URL

--- migration/src/routes/file_delete/entry.svelte
+++ migration/src/routes/file_delete/entry.svelte
@@ -10,7 +10,7 @@
 <div class="w-96">
     <input bind:value={entry.name} placeholder="Name*" class={"relative w-full h-9 field top " + type}>
     <input bind:value={entry.current_path} placeholder="Current Path (file path)" class={"relative w-full h-9 field middle " + type}>
-    <input bind:value={entry.redirect} placeholder="Redirect* (URI)" class={"relative w-full h-9 field bottom " + type}>
+    <input bind:value={entry.redirect} placeholder="Redirect* (URL)" class={"relative w-full h-9 field bottom " + type}>
 </div>
 
 <style lang="scss">
