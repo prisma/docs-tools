cd21b14c5f3ec17bce981002566386d96278c5fe
Bama <44502143+ObamaTheLlama114@users.noreply.github.com>
2022-10-19 14:47:39 -0500

    add-multi-db-support (#38)

--- migration/api/file_delete_paths.py
+++ migration/api/file_delete_paths.py
@@ -11,7 +11,12 @@ app = Flask(__name__)
 @app.route('/<path:path>', methods=['PUT', 'POST', 'GET', 'DELETE'])
 def catch_all(path):
     from flask import request
-
+    
+    collection = "file_delete_paths"
+    if request.headers.get("database"):
+        collection += "_"
+        collection += request.headers.get('database')
+    
     delete_type = {
         "_id": [str, NoneType],
         "name": [str, NoneType],
@@ -27,16 +32,15 @@ def catch_all(path):
         response = []
         for i in body:
             try:
-                client.data.file_delete_paths.insert_one(validate_type(i, delete_type))
+                client.data[collection].insert_one(validate_type(i, delete_type))
                 response.append("OK")
             except:
                 response.append("ERR")
-        client.data.changes.insert_one({})
         return Response(json.dumps(response), mimetype='application/json', status=200)
     
     elif request.method == 'GET':
         try:
-            return Response(json.dumps([{key:str(value) if type(value) is ObjectId else value for key, value in item.items()} for item in list(client.data.file_delete_paths.find(validate_query(body, delete_type)))]), mimetype='application/json')
+            return Response(json.dumps([{key:str(value) if type(value) is ObjectId else value for key, value in item.items()} for item in list(client.data[collection].find(validate_query(body, delete_type)))]), mimetype='application/json')
         except:
             return Response(json.dumps({"Error": "bad shape"}), mimetype='application/json')
     
@@ -44,11 +48,10 @@ def catch_all(path):
         data = [(validate_query(i["query"], delete_type), i["update"]) for i in body]
         response = []
         for query, update in data:
-            res = client.data.file_delete_paths.update_many(query, update)
+            res = client.data[collection].update_many(query, update)
             response.append({"matched_count": res.matched_count, "modified_count": res.modified_count})
         return Response(json.dumps(response), mimetype='application/json')
 
     elif request.method == 'DELETE':
-        data = client.data.file_delete_paths.delete_many(validate_query(body, delete_type))
-        client.data.changes.insert_one({})
+        data = client.data[collection].delete_many(validate_query(body, delete_type))
         return Response(json.dumps({"deleted": data.deleted_count}), mimetype='application/json')
--- migration/api/file_move_paths.py
+++ migration/api/file_move_paths.py
@@ -12,6 +12,11 @@ app = Flask(__name__)
 def catch_all(path):
     from flask import request
 
+    collection = "file_move_paths"
+    if request.headers.get("database"):
+        collection += "_"
+        collection += request.headers.get("database")
+
     move_type = {
         "_id": [str, NoneType],
         "name": [str, NoneType],
@@ -27,16 +32,15 @@ def catch_all(path):
         response = []
         for i in body:
             try:
-                client.data.file_move_paths.insert_one(validate_type(i, move_type))
+                client.data[collection].insert_one(validate_type(i, move_type))
                 response.append("OK")
             except:
                 response.append("ERR")
-        client.data.changes.insert_one({})
         return Response(json.dumps(response), mimetype='application/json', status=200)
     
     elif request.method == 'GET':
         try:
-            return Response(json.dumps([{key:str(value) if type(value) is ObjectId else value for key, value in item.items()} for item in list(client.data.file_move_paths.find(validate_query(body, move_type)))]), mimetype='application/json')
+            return Response(json.dumps([{key:str(value) if type(value) is ObjectId else value for key, value in item.items()} for item in list(client.data[collection].find(validate_query(body, move_type)))]), mimetype='application/json')
         except:
             return Response(json.dumps({"Error": "bad shape"}), mimetype='application/json')
 
@@ -44,11 +48,11 @@ def catch_all(path):
         data = [(validate_query(i["query"], move_type), i["update"]) for i in body]
         response = []
         for query, update in data:
-            res = client.data.file_move_paths.update_many(query, update)
+            res = client.data[collection].update_many(query, update)
             response.append({"matched_count": res.matched_count, "modified_count": res.modified_count})
         return Response(json.dumps(response), mimetype='application/json')
 
     elif request.method == 'DELETE':
-        data = client.data.file_move_paths.delete_many(validate_query(body, move_type))
+        data = client.data[collection].delete_many(validate_query(body, move_type))
         client.data.changes.insert_one({})
         return Response(json.dumps({"deleted": data.deleted_count}), mimetype='application/json')
