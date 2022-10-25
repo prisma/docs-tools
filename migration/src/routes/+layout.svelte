b1bc9da8018a610ac1da187fe0632b909ae4d015
ObamaTheLlama114 <william.chomel@gmail.com>
2022-10-25 13:22:32 -0500

    fix-styling-on-button

--- migration/src/routes/+layout.svelte
+++ migration/src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
     import Selector from "./selector.svelte";
     import {database, setDatabase} from "../global";
+    import "./entry.scss";
     let _database = database;
 </script>
 
 <Selector items={[{name: "File Move", link: "/file_move"}, {name: "File Delete", link: "/file_delete"}, {name: "File Surgery", link: "/file_surgery"}, {name: "File Stitch", link: "/file_stitch"}]}/>
-<input bind:value={_database} on:input={() => {setDatabase(_database)}}/>
+<input bind:value={_database} on:input={() => {setDatabase(_database)}} placeholder="Database* (Leave blank for default)" class={"relative w-full h-9 field button"} style={"width: 300px; margin: 8px; margin-left: 18px"}/>
 <slot></slot>
 
 <style lang="scss">
